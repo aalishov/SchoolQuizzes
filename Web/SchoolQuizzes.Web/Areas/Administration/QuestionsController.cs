e2f9bcefe5babcd5993768e6f07e7c4c2cae55d8
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-12-28 16:57:23 +0200

    Update QuestionsController!

--- Web/SchoolQuizzes.Web/Areas/Administration/QuestionsController.cs
+++ Web/SchoolQuizzes.Web/Areas/Administration/QuestionsController.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using SchoolQuizzes.Data;
-using SchoolQuizzes.Data.Models;
-using SchoolQuizzes.Web.Areas.Administration.Controllers;
-
-namespace SchoolQuizzes.Web.Areas.Administration
+﻿namespace SchoolQuizzes.Web.Areas.Administration
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.EntityFrameworkCore;
+    using SchoolQuizzes.Data;
+    using SchoolQuizzes.Data.Models;
+    using SchoolQuizzes.Web.Areas.Administration.Controllers;
+
     [Area("Administration")]
     public class QuestionsController : AdministrationController
     {
